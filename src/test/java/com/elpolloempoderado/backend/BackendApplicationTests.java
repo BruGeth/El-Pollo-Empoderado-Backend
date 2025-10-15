710a3d40f9026584fac48324c447f685a02d6b80
BruGeth <g15bru03@hotmail.com>
2025-10-15 00:36:15 -0500

    feat: agregar estructura inicial del proyecto con configuración de Maven y Spring Boot

---
+++ src/test/java/com/elpolloempoderado/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elpolloempoderado.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
